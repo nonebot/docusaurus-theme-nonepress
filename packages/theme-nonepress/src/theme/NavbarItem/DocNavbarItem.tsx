acdaae37131b45d0f957848e42af57e51d62d9a0
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-09-25 08:21:54 +0000

    :bug: Fix doc navbar item highlight error

--- packages/theme-nonepress/src/theme/NavbarItem/DocNavbarItem.tsx
+++ packages/theme-nonepress/src/theme/NavbarItem/DocNavbarItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useActiveDocContext } from "@docusaurus/plugin-content-docs/client";
+// import { useActiveDocContext } from "@docusaurus/plugin-content-docs/client";
 import { useLayoutDoc } from "@docusaurus/theme-common/internal";
 
 import DefaultNavbarItem from "@theme/NavbarItem/DefaultNavbarItem";
@@ -12,7 +12,7 @@ export default function DocNavbarItem({
   label: staticLabel,
   ...props
 }: Props): JSX.Element | null {
-  const { activeDoc } = useActiveDocContext(docsPluginId);
+  // const { activeDoc } = useActiveDocContext(docsPluginId);
   const doc = useLayoutDoc(docId, docsPluginId);
 
   // Draft items are not displayed in the navbar.
@@ -24,10 +24,10 @@ export default function DocNavbarItem({
     <DefaultNavbarItem
       exact
       {...props}
-      isActive={() =>
-        activeDoc?.path === doc.path ||
-        (!!activeDoc?.sidebar && activeDoc.sidebar === doc.sidebar)
-      }
+      // isActive={() =>
+      //   activeDoc?.path === doc.path ||
+      //   (!!activeDoc?.sidebar && activeDoc.sidebar === doc.sidebar)
+      // }
       label={staticLabel ?? doc.id}
       to={doc.path}
     />
