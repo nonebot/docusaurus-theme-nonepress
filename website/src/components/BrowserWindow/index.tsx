e3359f13693af887cc08d3c3d74e97f78ed80687
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-09-25 09:23:23 +0000

    :bug: Fix scrollbar color

--- website/src/components/BrowserWindow/index.tsx
+++ website/src/components/BrowserWindow/index.tsx
@@ -8,14 +8,14 @@ type Props = {
 export default function browserWindow(props: Props): JSX.Element {
   const { title, children } = props;
   return (
-    <div className="mockup-window border pt-0 before:content-none">
+    <div className="mockup-window border pt-0 before:content-none not-prose">
       <div className="flex gap-2 w-full px-4 py-2 bg-base-content/10">
         <div>
           <div className="bg-red-600 inline-block h-3 w-3 rounded-full mr-1"></div>
           <div className="bg-yellow-500 inline-block h-3 w-3 rounded-full mr-1"></div>
           <div className="bg-green-500 inline-block h-3 w-3 rounded-full"></div>
         </div>
-        <div className="flex-grow text-left inline-block bg-base-200 rounded-full px-2 truncate text-sm">
+        <div className="flex-grow text-left inline-block bg-base-200 rounded-full px-2 truncate text-sm transition-[background-color] duration-500">
           {title}
         </div>
       </div>
