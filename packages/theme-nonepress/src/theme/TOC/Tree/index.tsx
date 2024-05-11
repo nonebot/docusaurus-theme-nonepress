b759bd0b258b802fb22e69fec9da9ab0610c5999
yixiaojiu <yi_xiao_jiu@qq.com>
2024-05-11 21:21:39 +0800

    feat: :sparkles: optimize jump experience of TOC

--- packages/theme-nonepress/src/theme/TOC/Tree/index.tsx
+++ packages/theme-nonepress/src/theme/TOC/Tree/index.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import clsx from "clsx";
 
 import type { Props } from "@theme/TOC/Tree";
 
+const OFFSET = 70;
+
 // Recursive component rendering the toc tree
 function TOCTree({ toc, linkClassName }: Props): JSX.Element | null {
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      const eventTarget = event.currentTarget;
+      const targetEl = document.getElementById(
+        decodeURIComponent(eventTarget.href.split("#")[1]),
+      );
+      if (!targetEl) {
+        return;
+      }
+      const targetPadding = parseInt(
+        window.getComputedStyle(targetEl).paddingTop,
+        10,
+      );
+      const targetTop =
+        window.scrollY +
+        targetEl.getBoundingClientRect().top -
+        OFFSET +
+        targetPadding;
+
+      requestAnimationFrame(() => {
+        if (Math.abs(targetTop - window.scrollY) > window.innerHeight)
+          window.scrollTo(0, targetTop);
+        else window.scrollTo({ left: 0, top: targetTop, behavior: "smooth" });
+      });
+    },
+    [],
+  );
   if (!toc.length) {
     return null;
   }
+
   return (
     <ul className="toc-tree">
       {toc.map((heading) => (
         <li key={heading.id}>
           <a
             href={`#${heading.id}`}
+            onClick={handleClick}
             className={clsx("menu-link menu-item", linkClassName)}
           >
             {/* Developer provided the HTML, so assume it's safe. */}
