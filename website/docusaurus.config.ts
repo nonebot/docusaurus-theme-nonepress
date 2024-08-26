0b9c99ae726f0727cbcf63dcd64c56c57182892d
StarHeartHunt <starheart233@gmail.com>
2024-08-26 21:13:34 +0800

    :recycle: move to typescript config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,64 +1,98 @@
-// @ts-check
-
 import { themes } from "prism-react-renderer";
+
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@nullbot/docusaurus-preset-nonepress";
 
 const darkCodeTheme = themes.dracula;
 const lightCodeTheme = themes.github;
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: "NonePress",
-  tagline: "A Docusaurus theme for NoneBot",
-  favicon: "img/favicon.ico",
+// By default, we use Docusaurus Faster
+// DOCUSAURUS_SLOWER=true is useful for benchmarking faster against slower
+// hyperfine --prepare 'yarn clear:website' --runs 3 'DOCUSAURUS_SLOWER=true yarn build:website:fast' 'yarn build:website:fast'
+const isSlower = process.env.DOCUSAURUS_SLOWER === "true";
+if (isSlower) {
+  console.log("🐢 Using slower Docusaurus build");
+}
 
-  // Set the production url of your site here
-  url: "https://nonepress.nonebot.dev",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+export default async function createConfigAsync() {
+  return {
+    title: "NonePress",
+    tagline: "A Docusaurus theme for NoneBot",
+    favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "nonebot", // Usually your GitHub org/user name.
-  projectName: "docusaurus-theme-nonepress", // Usually your repo name.
+    // Set the production url of your site here
+    url: "https://nonepress.nonebot.dev",
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+    // future: {
+    //   experimental_faster: !isSlower,
+    // },
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "zh-Hans"],
-  },
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  presets: [
-    [
-      "@nullbot/docusaurus-preset-nonepress",
-      /** @type {import('../packages/preset-nonepress/lib/index').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl:
-            "https://github.com/nonebot/docusaurus-theme-nonepress/edit/master/website/",
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-        sitemap: {
-          changefreq: "daily",
-          priority: 0.5,
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "nonebot", // Usually your GitHub org/user name.
+    projectName: "docusaurus-theme-nonepress", // Usually your repo name.
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en", "zh-Hans"],
+    },
+
+    webpack: {
+      jsLoader: (isServer) => ({
+        loader: require.resolve("swc-loader"),
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+              tsx: true,
+            },
+            transform: {
+              react: {
+                runtime: "automatic",
+              },
+            },
+            target: "es2017",
+          },
+          module: {
+            type: isServer ? "commonjs" : "es6",
+          },
         },
       }),
-    ],
-  ],
+    },
 
-  themeConfig:
-    /** @type {import('../packages/preset-nonepress/lib/index').ThemeConfig} */
-    ({
+    presets: [
+      [
+        "@nullbot/docusaurus-preset-nonepress",
+        {
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl:
+              "https://github.com/nonebot/docusaurus-theme-nonepress/edit/master/website/",
+            showLastUpdateAuthor: true,
+            showLastUpdateTime: true,
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+          sitemap: {
+            changefreq: "daily",
+            priority: 0.5,
+          },
+        } satisfies Preset.Options,
+      ],
+    ],
+
+    themeConfig: {
       colorMode: {
         respectPrefersColorScheme: true,
       },
@@ -194,7 +228,6 @@ const config = {
           ],
         },
       },
-    }),
-};
-
-module.exports = config;
+    } satisfies Preset.ThemeConfig,
+  } satisfies Config;
+}
