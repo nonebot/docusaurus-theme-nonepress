d685585875968d7381a0d3108b57fd67041f8b02
StarHeartHunt <starheart233@gmail.com>
2026-02-13 22:30:45 +0800

    fix(theme): replace copy-text-to-clipboard by clipboard API

--- packages/theme-nonepress/src/theme/CodeBlock/CopyButton/index.tsx
+++ packages/theme-nonepress/src/theme/CodeBlock/CopyButton/index.tsx
@@ -9,21 +9,44 @@ import React, {
 import clsx from "clsx";
 
 import { translate } from "@docusaurus/Translate";
-import copy from "copy-text-to-clipboard";
 
 import type { Props } from "@theme/CodeBlock/CopyButton";
 import IconCopy from "@theme/Icon/Copy";
 import IconSuccess from "@theme/Icon/Success";
 
+function title() {
+  return translate({
+    id: "theme.CodeBlock.copy",
+    message: "Copy",
+    description: "The copy button label on code blocks",
+  });
+}
+
+function ariaLabel(isCopied: boolean) {
+  return isCopied
+    ? translate({
+        id: "theme.CodeBlock.copied",
+        message: "Copied",
+        description: "The copied button label on code blocks",
+      })
+    : translate({
+        id: "theme.CodeBlock.copyButtonAriaLabel",
+        message: "Copy code to clipboard",
+        description: "The ARIA label for copy code blocks button",
+      });
+}
+
 export default function CopyButton({ code, className }: Props): ReactNode {
   const [isCopied, setIsCopied] = useState(false);
   const copyTimeout = useRef<number | undefined>(undefined);
-  const handleCopyCode = useCallback(() => {
-    copy(code);
-    setIsCopied(true);
-    copyTimeout.current = window.setTimeout(() => {
-      setIsCopied(false);
-    }, 1000);
+
+  const copyCode = useCallback(() => {
+    navigator.clipboard.writeText(code).then(() => {
+      setIsCopied(true);
+      copyTimeout.current = window.setTimeout(() => {
+        setIsCopied(false);
+      }, 1000);
+    });
   }, [code]);
 
   useEffect(() => () => window.clearTimeout(copyTimeout.current), []);
@@ -31,30 +54,14 @@ export default function CopyButton({ code, className }: Props): ReactNode {
   return (
     <button
       type="button"
-      aria-label={
-        isCopied
-          ? translate({
-              id: "theme.CodeBlock.copied",
-              message: "Copied",
-              description: "The copied button label on code blocks",
-            })
-          : translate({
-              id: "theme.CodeBlock.copyButtonAriaLabel",
-              message: "Copy code to clipboard",
-              description: "The ARIA label for copy code blocks button",
-            })
-      }
-      title={translate({
-        id: "theme.CodeBlock.copy",
-        message: "Copy",
-        description: "The copy button label on code blocks",
-      })}
+      aria-label={ariaLabel(isCopied)}
+      title={title()}
       className={clsx(
         "btn btn-square btn-sm no-animation swap swap-rotate code-block-btn",
         className,
         isCopied && "swap-active",
       )}
-      onClick={handleCopyCode}
+      onClick={copyCode}
     >
       <IconCopy className="swap-off code-block-btn-icon" />
       <IconSuccess className="swap-on code-block-btn-icon text-success" />
